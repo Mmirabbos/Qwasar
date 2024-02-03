f290f7b8b6d8776809c9f0f52b627806660aa348
Mirabbos <mirabbosminavarov@gmail.com>
2024-02-03 10:21:06 +0500

    My_M_And_A

--- m_and_a.py
+++ m_and_a.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from tabulate import tabulate
+import os
 
-data_1 = r'C:\Users\mirabbos\Desktop\M_And_A\only_wood_customer_us_1.csv'
-data_2 = r'C:\Users\mirabbos\Desktop\M_And_A\only_wood_customer_us_2.csv'
-data_3 = r'C:\Users\mirabbos\Desktop\M_And_A\only_wood_customer_us_3.csv'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# print(BASE_DIR)
+data_1 = BASE_DIR + '/only_wood_customer_us_1.csv'
+data_2 = BASE_DIR + '/only_wood_customer_us_2.csv'
+data_3 = BASE_DIR + '/only_wood_customer_us_3.csv'
 
 def load_dataset_1(path):
     return pd.read_csv(path)
@@ -29,11 +32,11 @@ def cleaning_string_integer_3(df):
 
 def clean_dataset_1(df):
     df['Gender'] = df['Gender'].apply(clean_gender)
-    df['FirstName'] = df['FirstName'].str.title()
-    df['LastName'] = df['LastName'].str.title()
+    df['FirstName'] = df['FirstName'].apply(lambda x: x.replace("\\", "").replace('"', "").title() if isinstance(x, str) else x)
+    df['LastName'] = df['LastName'].apply(lambda x: x.replace("\\", "").replace('"', ""))
     df['UserName'] = df['UserName'].str.lower()
     df['Email'] = df['Email'].apply(lambda row: row.lower()+'.com' if row[-4:].lower()!='.com' else row.lower())
-    df['City'] = df['City'].str.title()
+    df['City'] = df['City'].apply(lambda x: x.replace("_", " ").replace('-', " ")).str.title()
     df['Country'] = 'USA'
     return df
 
@@ -43,18 +46,18 @@ def clean_dataset_2(df):
     df["Gender"].replace({"0": "Male", "1": "Female", "M": "Male", "F": "Female"}, inplace=True)
     df["FullName"] = df["FullName"].apply(lambda x: x.replace("\\", "").replace('"', "")).str.title()
     df["Email"] = df["Email"].str.lower()
+    df['Country'] = 'USA'
 
     return df
     
 
 def clean_dataset_3(df):
-    df["Gender"] = df['Gender'].map(lambda x: x.replace("string_", "").replace("boolean_", "").replace("integer_", "")).map(lambda x: x.replace("0", "Male").replace("1", "Female"))
+    df["Gender"] = df['Gender'].replace({"string_": "", "boolean_": "", "integer_": "", "0": "Male", "1": "Female", "_": "", "Characterm": "nan"}, regex=True).str.title()
     df["Name"] = df["Name"].apply(lambda x: x.replace("string_", "").replace('"', "")).str.title()
     df["Email"] = df["Email"].astype(str).apply(lambda x: x.replace("string_", "").lower())
     df["Age"] = df["Age"].apply(lambda x: x.replace("integer_", "").replace("year", ""))
     df["City"] = df["City"].apply(lambda x: x.replace("string_", "")).str.title()
-    df["Country"] = df["Country"].apply(lambda x: x.replace("string_United_State_Of_America", "USA").replace("string_Us", "USA").replace("string_U.S.A.", "USA").replace("string_1", "NONE").replace("string_u.s.", "USA").replace("string_US", "USA").replace("string_U.S.", "USA").replace("string_united state of america", "USA").replace("string_U.s.a.", "USA"))
-
+    df["Country"] = "USA"
     return df
 
 df_1 = load_dataset_1(data_1)
@@ -65,11 +68,18 @@ clean_df_1 = clean_dataset_1(df_1)
 clean_df_2 = clean_dataset_2(df_2)
 clean_df_3 = clean_dataset_3(df_3)
 
-print("Cleaned DataFrame 1:")
-pretty_print(clean_df_1)
+# print("Cleaned DataFrame 1:")
+# pretty_print(clean_df_1)
 
-print("\nCleaned DataFrame 2:")
-pretty_print(clean_df_2)
+# print("\nCleaned DataFrame 2:")
+# pretty_print(clean_df_2)
 
-print("\nCleaned DataFrame 3:")
-pretty_print(clean_df_3)
+# print("\nCleaned DataFrame 3:")
+# pretty_print(clean_df_3)
+
+merged_df = pd.concat([clean_df_1, clean_df_2, clean_df_3], ignore_index=True)
+
+order = ['FirstName', 'LastName', 'FullName', 'Age', 'Gender', 'UserName', 'Email', 'City', 'Country']
+selected_columns = merged_df[order]
+
+selected_columns.to_excel('merged.xlsx', index=False)
